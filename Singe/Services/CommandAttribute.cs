b3997034c1557a9b690939b198ef200626fac39a
Ryan Andersen <redninja106@live.com>
2021-03-11 10:31:39 -0500

    Add CommandAttribute class

---
+++ Singe/Services/CommandAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singe.Services.old
+{
+    public sealed class CommandAttribute : Attribute
+    {
+        public CommandAttribute()
+        {
+
+        }
+
+        public string Namespace { get; set; }
+    }
+}
