b8070f233f08733dd61c8e342ddd9f743c167205
Ryan Andersen <redninja106@live.com>
2021-03-11 10:23:35 -0500

    move to "old' namespace to prevent conflicts

--- Singe/Services/old/ContentLoader.cs
+++ Singe/Services/old/ContentLoader.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Singe.Services
+namespace Singe.Services.old
 {
     class ContentLoader
     {
--- Singe/Services/old/Service.cs
+++ Singe/Services/old/Service.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 
-namespace Singe.Services
+namespace Singe.Services.old
 {
     public class Service
     {
